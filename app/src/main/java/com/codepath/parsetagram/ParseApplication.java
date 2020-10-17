866b22bc3a82abdd6cadb20e64177cb75bc91934
Wallace-GS <wgsantos29@gmail.com>
2020-10-17 15:59:58 -0700

    sets up parse application

--- app/src/main/java/com/codepath/parsetagram/ParseApplication.java
+++ app/src/main/java/com/codepath/parsetagram/ParseApplication.java
@@ -3,11 +3,14 @@ package com.codepath.parsetagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("2IFaYJgtQcmGqrgNwGbG2iOn9JUhbMUrY5mdvPlA")
